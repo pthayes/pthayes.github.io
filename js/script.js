40bf99f0eb31e67f476378d0895a914d04819532
vbarbosavaz <v.barbosavaz@gmail.com>
2023-04-12 18:06:42 +1000

    Add modal

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+document.getElementById('my-modal').innerHTML = `<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel"></h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+        <img id="imageresource" src="../img/punk-pavilion-model-3.jpg" style="width: 100%;">
+      </div>
+      <!-- <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div> -->
+    </div>
+  </div>
+</div>
+`;
+
+
+var exampleModal = document.getElementById('exampleModal')
+exampleModal.addEventListener('show.bs.modal', function (event) {
+  // Button that triggered the modal
+  var button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  var img_src = button.querySelector('img').getAttribute('src')
+  var img_txt = button.querySelector('p').innerText
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  var modalTitle = exampleModal.querySelector('.modal-title')
+  var modalBodyInput = exampleModal.querySelector('.modal-body img')
+
+  modalTitle.textContent = img_txt
+  modalBodyInput.src = img_src
+})
